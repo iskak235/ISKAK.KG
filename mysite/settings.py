74247394125575c602cd753d009e298020d3ccf5
iskak235 <iskakzoldubaj5@gmail.com>
2026-03-08 09:48:46 +0600

    Fix middleware and project name for render

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Бул жерде турат
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
